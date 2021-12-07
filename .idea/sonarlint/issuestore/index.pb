cd963e72d18bc448f55f6f06e1390a6ee14d4604
Daniel Shatz <sbhtkxpto@gmail.com>
2021-12-07 04:43:38 -0800

    added key pair creation

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,6 +1,4 @@
 
-L
-.terraform/terraform.tfstate,b/c/bce8dd1a5530836070c1a4b26ae53ef6ee1cf5e5
 :
 
 backend.tf,5/f/5f6a254a3388a8aec4e31a860f8e15c79ae8f00e
--- instances.tf
+++ instances.tf
@@ -6,3 +6,15 @@ data "aws_ssm_parameter" "linuxAmiOregon" {
   name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
   provider = aws.region-worker
 }
+
+resource "aws_key_pair" "master-key" {
+  provider   = aws.region-master
+  key_name   = "jenkins"
+  public_key = file("/home/daniel/.ssh/id_rsa.pub")
+}
+
+resource "aws_key_pair" "worker-key" {
+  provider   = aws.region-worker
+  key_name   = "jenkins"
+  public_key = file("/home/daniel/.ssh/id_rsa.pub")
+}
